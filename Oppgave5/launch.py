02c2006d5c9adae01be6ccf4b99728d76fcfd4ce
vebovs <vebovs>
2019-10-11 19:59:16 +0200

    simulert oppskytning med figur

--- Oppgave5/launch.py
+++ Oppgave5/launch.py
@@ -2,6 +2,9 @@ import numpy as np
 import math
 from SaturnV import SaturnV
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from PIL import Image
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 
 earth_mass = 5.9736 * (10**24) # Jordens masse i kilogram
 earth_radius = (12756.28/2) * 1000 # Jordens radius ved ekvator i meter
@@ -32,6 +35,7 @@ saturnV = SaturnV(m1,c1,b1,t1,m2,c2,b2,t2,m3,c3,b3,t3)
 
 time_stage_one = b1
 time_stage_two = b1 + b2
+time_stage_three = b1 + b2 + b3
 
 def height(t, v):
     return (1/2)*(v[t] + v[t - 1])*t
@@ -84,12 +88,31 @@ def Fd(t, a, v):
     return (1/2)*(1/2)*density(t, v)*area(t)*(speed(t, a, v)**2)
 
 
+def imscatter(x, y, image, ax=None, zoom=1):
+    if ax is None:
+        ax = plt.gca()
+    try:
+        image = plt.imread(image)
+    except TypeError:
+        # Likely already an array...
+        pass
+    im = OffsetImage(image, zoom=zoom)
+    x, y = np.atleast_1d(x, y)
+    artists = []
+    for x0, y0 in zip(x, y):
+        ab = AnnotationBbox(im, (x0, y0), xycoords='data', frameon=False)
+        artists.append(ax.add_artist(ab))
+    ax.update_datalim(np.column_stack([x, y]))
+    ax.autoscale()
+    return artists
+
+
 def main():
     # Antall tidstrinn
     steps = b1 + b2 + b3
 
     # Initialiserer startverdier
-    h = 1
+    h = 0.001 # Initialiseres med en verdi større enn null for å komme inn i løkken. Verdien påvirker ingen av kalkulasjonene.
     F = []
     a = []
     m = []
@@ -99,16 +122,32 @@ def main():
     F.append(Fs(0) - gravity_constant * ((earth_mass * m[0])/(earth_radius**2)))
     a.append(F[0] / m[0])
     t = 1
+    img_path_rocket_down = '/home/vebovs/Desktop/fysikk-og-matematikk/Oppgave5/rocket_down.png'
+    img_path_rocket_up_no_fuel = '/home/vebovs/Desktop/fysikk-og-matematikk/Oppgave5/rocket_up_no_fuel.png'
+    img_path_rocket_up = '/home/vebovs/Desktop/fysikk-og-matematikk/Oppgave5/rocket_up.png'
+    img_path = img_path_rocket_up
+    down = False
     while h >= 0:
+        last_h = h
         v.append(speed(t, a, v))
         F.append(Fs(t) - (Fg(t, v) + Fd(t, a, v)))
         m.append(mass(t))
         a.append(F[t] / m[t])
         h = height(t, v)
-        plt.scatter(0, h)
-        plt.pause(0.05)
+        plt.clf()
+        plt.xlim(-500, 500)
+        plt.ylim(0, 3000000)
+        plt.ylabel('Høyde (m)')
+        if t > time_stage_three and not down:
+            img_path = img_path_rocket_up_no_fuel
+        if last_h > h and not down:
+            img_path = img_path_rocket_down
+            down = True
+        img = OffsetImage(plt.imread(img_path))
+        img = mpimg.imread(img_path)
+        plt.imshow(img, aspect = 'auto', extent = [-40, 40, h - 100000, h + 200000])
+        plt.pause(0.000000001)
         t = t + 1
-        print("Height: ", h)
 
     plt.show()
 
